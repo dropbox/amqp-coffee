f9cd5c5d064a5df62f715603106c6a51f8bdad0c
AVVS <v@makeomatic.ru>
2018-06-07 20:14:51 -0700

    chore: improve custom error

--- src/lib/Errors.js
+++ src/lib/Errors.js
@@ -1,21 +1,24 @@
 /**
  * This file contains custom error implementation to fix string based errors
  */
+const util = require('util');
 
 function BasicReturnError(err) {
   this.code = 'ERR_AMQP_BASIC_RETURN';
+  this.name = 'AMQP_BasicReturnError';
 
   // assign properties
-  this.message = err.replyText;
   this.replyCode = err.replyCode;
   this.exchange= err.exchange;
   this.routingKey = err.routingKey;
 
   // preserve back-compatibility
   this.replyText = err.replyText;
+  this.message = err.replyText;
 
-  // NOTE: we may want to capture stack trace later, so far don't do it
-  // Error.captureStackTrace(this, BasicReturnError);
+  // and capture stack trace to make it useful
+  Error.captureStackTrace(this, BasicReturnError);
 }
+util.inherits(BasicReturnError, Error);
 
 exports.BasicReturnError = BasicReturnError;
